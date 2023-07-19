27731856f4a1375ca3b51a91d3c06d9fb86166e7
Why070 <135810436+Why070@users.noreply.github.com>
2023-07-19 13:44:04 +0800

    Update trainer.py

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2809,12 +2809,9 @@ class Trainer:
         print(get_memory())
         print(get_gpu_memory_usage())
 
-        print(model.forward.__code__.co_varnames)
-
         for intermediate_output in intermediate_outputs:
             print(intermediate_output)
-            logits = intermediate_output.logits
-            print("logits.size:", logits.size(), "logits.dtype:", logits.dtype)
+            
 
         # 注销钩子函数
         for handle in hook_handles:
