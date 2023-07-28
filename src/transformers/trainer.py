4c49668562c915fb34e1530cf03135479b5fe11e
Why070 <135810436+Why070@users.noreply.github.com>
2023-07-28 11:03:27 +0800

    Update trainer.py

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2826,12 +2826,7 @@ class Trainer:
             if isinstance(intermediate_output, torch.Tensor):  # 检查是否是张量
                 print(f"模块 '{module_name}' 输出大小:", intermediate_output.shape, "输出数据类型:", intermediate_output.dtype)
             else:
-                if hasattr(intermediate_output, 'last_hidden_state'):  # Check for the 'last_hidden_state' attribute
-                    last_hidden_state = intermediate_output.last_hidden_state
-                    print(f"模块 '{module_name}' 中间输出 last_hidden_state 大小:", last_hidden_state.shape, "输出数据类型:", last_hidden_state.dtype)
-                if hasattr(intermediate_output, 'logits'):  # Check for the 'logits' attribute
-                    logits = intermediate_output.logits
-                    print(f"模块 '{module_name}' 中间输出 logits 大小:", logits.shape, "输出数据类型:", logits.dtype)
+                print(intermediate_output)
 
         # 注销钩子函数
         for handle in hook_handles:
